c1df3b8d1afcac0680b82947b8418d9178879826
Alex Charlton <alex.n.charlton@gmail.com>
2024-08-03 15:54:41 -0700

    Add missing Warn LogLevel

--- vosk/src/log.rs
+++ vosk/src/log.rs
@@ -4,12 +4,15 @@ use vosk_sys::*;
 #[derive(Debug, Default, Clone, Copy)]
 /// Log level for Kaldi messages.
 pub enum LogLevel {
-    /// Print Error, Info and Debug messages.
-    ErrorInfoDebug,
+    /// Print Error, Warn, Info and Debug messages.
+    Debug,
 
-    /// Print Error and Info, but not Debug messages (default).
+    /// Print Error, Warn, and Info, but not Debug messages (default).
     #[default]
-    ErrorInfo,
+    Info,
+
+    /// Print Error and Warn messages.
+    Warn,
 
     /// Only print Error messages.
     Error,
@@ -18,16 +21,17 @@ pub enum LogLevel {
 impl LogLevel {
     pub(self) fn to_c_int(self) -> c_int {
         match self {
-            Self::ErrorInfo => 0,
-            Self::Error => -1,
-            Self::ErrorInfoDebug => 1,
+            Self::Debug => 1,
+            Self::Info => 0,
+            Self::Warn => -1,
+            Self::Error => -2,
         }
     }
 }
 
 /// Set log level for Kaldi messages.
 ///
-/// Default: [`LogLevel::ErrorInfo`].
+/// Default: [`LogLevel::Info`].
 pub fn set_log_level(log_level: LogLevel) {
     unsafe { vosk_set_log_level(log_level.to_c_int()) }
 }
