82608a934ba0b7163222638036ad51664d5cd785
lightningpwr28 <lightningpwr28@gmail.com>
2024-08-15 17:09:56 -0700

    Added in-file modules for batch operations

--- vosk/src/lib.rs
+++ vosk/src/lib.rs
@@ -20,16 +20,12 @@ pub use recognition::*;
 /// Must be called once from the main thread.
 #[cfg(feature = "cuda")]
 pub fn gpu_init() {
-    unsafe {
-        vosk_sys::vosk_gpu_init()
-    }
+    unsafe { vosk_sys::vosk_gpu_init() }
 }
 
 /// Init CUDA device in a multi-threaded environment.
 /// Must be called for each thread.
 #[cfg(feature = "cuda")]
 pub fn gpu_thread_init() {
-    unsafe {
-        vosk_sys::vosk_gpu_thread_init()
-    }
+    unsafe { vosk_sys::vosk_gpu_thread_init() }
 }
