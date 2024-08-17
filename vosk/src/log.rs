2ceb01ac71e3899cc079d6bfe60ba5e5f1ce0eb3
Alex Charlton <alex.n.charlton@gmail.com>
2024-08-17 05:56:33 -0700

    Fix LogLevels (#9)
    * Add missing Warn LogLevel
    
    * Remove "Debug" log level, which has no upstream basis

--- vosk/src/log.rs
+++ vosk/src/log.rs
@@ -4,12 +4,12 @@ use vosk_sys::*;
 #[derive(Debug, Default, Clone, Copy)]
 /// Log level for Kaldi messages.
 pub enum LogLevel {
-    /// Print Error, Info and Debug messages.
-    ErrorInfoDebug,
-
-    /// Print Error and Info, but not Debug messages (default).
+    /// Print Error, Warn, and Info (default)
     #[default]
-    ErrorInfo,
+    Info,
+
+    /// Print Error and Warn messages.
+    Warn,
 
     /// Only print Error messages.
     Error,
@@ -18,16 +18,16 @@ pub enum LogLevel {
 impl LogLevel {
     pub(self) fn to_c_int(self) -> c_int {
         match self {
-            Self::ErrorInfo => 0,
-            Self::Error => -1,
-            Self::ErrorInfoDebug => 1,
+            Self::Info => 0,
+            Self::Warn => -1,
+            Self::Error => -2,
         }
     }
 }
 
 /// Set log level for Kaldi messages.
 ///
-/// Default: [`LogLevel::ErrorInfo`].
+/// Default: [`LogLevel::Info`].
 pub fn set_log_level(log_level: LogLevel) {
     unsafe { vosk_set_log_level(log_level.to_c_int()) }
 }
